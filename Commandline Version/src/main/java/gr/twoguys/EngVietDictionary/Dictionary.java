c907a28492a36cc688e7a0885e5e80dd12a767f1
PhongVan <fmlphongvan@gmail.com>
2021-09-29 21:28:12 +0700

    Add first version of DictionaryCMD, using Gradle to build

---
+++ Commandline Version/src/main/java/gr/twoguys/EngVietDictionary/Dictionary.java
@@ -0,0 +1,9 @@
+package gr.twoguys.EngVietDictionary;
+
+public class Dictionary {
+
+    public static void main(String[] args) {
+        DictionaryManagement.insertFromCommandline();
+        DictionaryManagement.showAllWord();
+    }
+}
